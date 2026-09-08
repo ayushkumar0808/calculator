c7d5d37fb569323942df27709cbc391d156ea824
Ayush Kumar <kayush3647@gmail.com>
2026-09-08 16:58:32 +0530

    add

--- script/script.js
+++ script/script.js
@@ -1,2 +1,17 @@
 let currentValue = "";
 document.querySelector("#text_dis").value = currentValue;
+
+function appendValue(value) {
+  currentValue = currentValue + value;
+  document.querySelector("#text_dis").value = currentValue;
+}
+
+function clearDisplay() {
+  currentValue = "";
+  document.querySelector("#text_dis").value = currentValue;
+}
+
+function calculator() {
+  currentValue = eval(currentValue);
+  document.querySelector("#text_dis").value = currentValue;
+}
